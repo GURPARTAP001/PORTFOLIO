45077e3c7f8a67b3b5fff235bda1b6ea76e3040b
Gurpartap <gurpartap6125@gmail.com>
2023-04-06 07:48:22 +0530

    made the skills and the qualification section of the webpage

--- script.js
+++ script.js
@@ -32,9 +32,47 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent=document.getElementsByClassName('skills__content');
+const skillsHeader=document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    // finding the accordion to be opened
+    let itemClass=this.parentNode.className
+
+    //closing the all open accordions
+    for(i=0;i<skillsContent.length;i++){
+        skillsContent[i].className='skills__content skills__close'
+    }
+
+    //add the open class to the accordion to be opened
+    if(itemClass==='skills__content skills__close'){
+        this.parentNode.className='skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click',toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs=document.querySelectorAll('[data-target]')
+const tabContents=document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        const target=document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent=>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
